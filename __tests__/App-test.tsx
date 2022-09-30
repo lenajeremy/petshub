f6db1222e782e3f096ac4d94838bd7ecb264341e
Jeremiah Lena <101596542+jeremiahFI@users.noreply.github.com>
2022-09-30 12:58:26 +0100

    built release apk

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -6,13 +6,14 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
-import { render } from '@testing-library/react-native';
+import { render, fireEvent, act } from '@testing-library/react-native';
 
 test('App Renders Correctly', () => {
-  // render(<App />);
+  render(<App />);
   expect(2 + 2).toEqual(4);
 });
  
 test('something happens', () => {
   expect(true).toBe(true);
+
 });
